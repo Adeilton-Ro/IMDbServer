0406c15147a17bb65410c7a3ef26e860cf6bac00
adeilton rodrigues <adeiltonjr15@gmail.com>
2022-08-29 16:28:42 -0300

    Adicionando ErrorHandlingBehaviour

---
+++ src/IMDb.Application/Behaviours/ErrorHandlingBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using FluentResults;
+using IMDb.Application.Extension;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace IMDb.Application.Behaviours;
+
+public class ErrorHandlingBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TResponse : ResultBase, new()
+    where TRequest : IRequest<TResponse>
+{
+    private readonly ILogger<ErrorHandlingBehaviour<TRequest, TResponse>> logger;
+
+    public ErrorHandlingBehaviour(ILogger<ErrorHandlingBehaviour<TRequest, TResponse>> logger)
+    {
+        this.logger = logger;
+    }
+    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    {
+        try
+        {
+            return await next();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "UnKnow Error");
+            return Result.Fail(new Error("Unknow Error")).To<TResponse>();
+        }
+    }
+}
